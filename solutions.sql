6c27c29ae09c794efb3fe2eac346f35f5da33db3
vielleicht-07 <ruben1110ik@gmail.com>
2025-01-31 23:08:02 +0100

    upload

--- solutions.sql
+++ solutions.sql
@@ -1,1 +1,67 @@
 -- Add you solution queries below:
+SE sakila;
+
+
+# 1: Write a query to display for each store its store ID, city, and country.
+SELECT s.store_id, c.city, co.country
+FROM store s
+JOIN address a ON s.address_id = a.address_id
+JOIN city c ON a.city_id = c.city_id
+JOIN country co ON c.country_id = co.country_id;
+
+#2: Write a query to display how much business, in dollars, each store brought in.
+SELECT s.store_id, SUM(p.amount) AS total_business
+FROM store s
+JOIN customer c ON s.store_id = c.store_id
+JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY s.store_id;
+
+
+#3: What is the average running time of films by category?
+SELECT c.name AS category, ROUND(AVG(f.length), 2) AS average_runtime
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY average_runtime DESC;
+
+
+#4: Which film categories are longest?
+SELECT c.name AS category, ROUND(AVG(f.length), 2) AS average_runtime
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY average_runtime DESC
+LIMIT 5;   
+
+
+#5: Display the most frequently rented movies in descending order.
+SELECT f.title, COUNT(r.rental_id) AS rental_count
+FROM rental r
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY f.title
+ORDER BY rental_count DESC
+LIMIT 5;   
+
+
+#6: List the top five genres in gross revenue in descending order.
+SELECT c.name AS genre, SUM(p.amount) AS total_revenue
+FROM payment p
+JOIN rental r ON p.rental_id = r.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY total_revenue DESC
+LIMIT 5;
+
+
+#7: Is "Academy Dinosaur" available for rent from Store 1?
+SELECT f.title, i.inventory_id
+FROM inventory i
+JOIN film f ON i.film_id = f.film_id
+WHERE f.title = 'Academy Dinosaur'
+AND i.store_id = 1;
